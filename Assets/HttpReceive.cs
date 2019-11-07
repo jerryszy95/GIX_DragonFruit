fdfe9c9fecc563eccc7279d9f4b304026aba4bbc
Zeyu Shen <jerryszy95@gmail.com>
2019-11-07 15:19:48 -0800

    Add Http receive

---
+++ Assets/HttpReceive.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HttpReceive : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        // A correct website page.
+        StartCoroutine(GetRequest("http://176.122.186.190/?get_info"));
+        
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.Log(pages[page] + ": Error: " + webRequest.error);
+            }
+            else
+            {
+                //Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                GameObject.Find("HttpText").GetComponent<Text>().text = webRequest.downloadHandler.text;
+            }
+
+        }
+    }
+}
